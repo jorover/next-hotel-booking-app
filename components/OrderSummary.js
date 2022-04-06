7d6c0be2117ef7f59b3eda9378ff44cd5c017906
UJ DEV <uj@ujdev.com>
2022-04-06 01:09:09 +0100

    updated

--- components/OrderSummary.js
+++ components/OrderSummary.js
@@ -4,28 +4,28 @@ const OrderSummary = ( { checkInDate, checkOutDate, dayNum, subTotal, total, roo
   return (
     <section className="ordersummarycontent">
         <h2> Order Summary </h2>
-        <article className="orderdetails">
+        <article className="orderdetails innerRoomDetails">
            <Room roomtype = {roomTypes} />
         </article>
-        <article className="orderdetails">
+        <article className="orderdetails innerRoomDetails">
             <p> CheckIn Date </p>
             <h5> {checkInDate} </h5>
         </article>
 
-        <article className="orderdetails">
+        <article className="orderdetails innerRoomDetails">
             <p> CheckOut Date </p>
             <h5> {checkOutDate} </h5>
         </article>
-        <article className="orderdetails">
+        <article className="orderdetails innerRoomDetails">
             <p> No. of nights </p>
             <h5> {dayNum} </h5>
         </article>
-        <article className="orderdetails">
+        <article className="orderdetails innerRoomDetails">
             <p> SubTotal(Tax 12%)  </p>
             <h5> ${subTotal} </h5>
         </article>
 
-        <article className="orderdetails">
+        <article className="orderdetails innerRoomDetails">
             <h5> Total  </h5>
             <h5> ${total} </h5>
         </article>
